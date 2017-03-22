bdd12fa2baf875bb72f8fc388b88298024c836c9
Arthur Fauquenot <arthurfauquenot@yahoo.com>
2017-03-22 09:19:59 +0100

    Simple ToDo List implementation / use of handlebars

--- app.js
+++ app.js
@@ -1,60 +1,59 @@
 // Required dependencies
 var http = require('http'),
     express = require('express'),
-    cookieSession = require('cookie-session'),
     bodyParser = require('body-parser'),
-    fs = require('fs');
+    cookieParser = require('cookie-parser'),
+    session = require('express-session'),
+    hbs = require('hbs');
 
-var urlEncodedParser = bodyParser.urlencoded({ extended: false });
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 // Variable initialization
-var app = express(),
-    server = http.createServer(app),
-    io = require('socket.io').listen(server);
+var app = express();
+var server = http.createServer(app);
+// var io = require('socket.io').listen(server);
 
-// Express server initialization
-app.use(cookieSession({
-        name: 'session',
-        secret: 'secretKey'
-    }))
+app.set('view engine', 'html')
+    .engine('html', hbs.__express)
     .use(express.static(__dirname + '/public'))
-    .use('/bower_components', express.static(__dirname + '/bower_components'))
-    .use(function(req, res, next) {
-        if (typeof(req.session.todolist) == 'undefined') {
+    .use('/bower_components', express.static(__dirname + '/bower_components/'))
+    .use(session({
+        secret: "todo-secret",
+        resave: false,
+        saveUninitialized: true
+    }))
+    .get('/todolist', function(req, res) {
+        if (typeof req.session.todolist == 'undefined') {
             req.session.todolist = [];
         }
+
+        res.render('index', { tasks: req.session.todolist });
+    })
+    .post('/todolist/add', urlencodedParser, function(req, res) {
+        var newTask = req.body.newTask;
+
+        if (newTask !== null) {
+            req.session.todolist.push(newTask);
+        }
+
+        res.redirect('/todolist');
+    })
+    .get('/todolist/delete/:id', function(req, res) {
+        req.session.todolist.splice(req.params.id, 1);
+
+        res.redirect('/todolist');
+    })
+    .use('/', function(req, res, next) {
+        res.redirect('/todolist');
+
         next();
-    })
-    .get('/todo', function(req, res) {
-        res.render('todolist.ejs', { todolist: req.session.todolist });
-    })
-    .post('/todo/add', urlEncodedParser, function(req, res) {
-        if (req.body.newTask !== '') {
-            req.session.todolist.push(req.body.newTask);
-        }
-        res.redirect('/todo');
-    })
-    .get('/todo/delete/:id', function(req, res) {
-        if (req.params.id !== '') {
-            req.session.todolist.splice(req.params.id, 1);
-        }
-        res.redirect('/todo');
-    })
-    .use(function(req, res, next) {
-        res.redirect('/todo');
     });
 
-
-// Real time communication with Socket.IO
-io.sockets.on('connection', function(socket) {
-
-    socket.on('newTask', function(task) {
-        socket.broadcast.emit('newTask', task);
-    });
-
-});
+// io.on('connection', function(socket) {
+//     console.log('Listening on : 8080');
+// });
 
 // Run the server on port 8080
-server.listen(8080, function() {
+app.listen(8080, function() {
     console.log('Application listening on port 8080 !');
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,3 @@
 $(document).ready(function() {
-
-    var socket = io('http://localhost:8080');
-
-    socket.on('newTask', function(data) {
-        console.log(data);
-    });
-
-    $('#taskForm').submit(function(event) {
-        var inputContent = $(this).find('input').val();
-
-        if (!inputContent || inputContent === '') {
-
-        } else {
-            socket.emit('newTask', inputContent);
-        }
-    });
+    // var socket = io('http://localhost:8080');
 });
